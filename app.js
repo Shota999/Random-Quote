26f78abb93bb349b799455eb462de092ab4da471
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-08-31 12:06:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// const quote = [1 , 2 , 3 , 4 , 5];
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const btn = document.getElementById("button");
+const url = "https://api.quotable.io/random";
+
+
+let getQuote = () => {
+    fetch(url)
+        .then((item) => item.json())
+        .then((item) => {
+            quote.innerText = item.content;
+            author.innerText = item.author;
+        });
+};
+
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
